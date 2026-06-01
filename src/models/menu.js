ee5c74a54715390f00c36cd9c39cf74412176e9a
Anton Sheenson <sheeeenson@gmail.com>
2026-06-01 14:44:18 +0400

    Restore app sections compatibility

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,6 +1,9 @@
+const APP_SECTION_VALUES = ['content', 'layoutPrint'];
+
 export const APP_SECTIONS = Object.freeze({
   CONTENT: 'content',
   LAYOUT_PRINT: 'layoutPrint',
+  includes: (section) => APP_SECTION_VALUES.includes(section),
 });
 
 export const BADGE_TYPES = Object.freeze(['New', 'Hit', 'Top', 'Promo', 'Spicy', 'Vegan', 'Custom']);
