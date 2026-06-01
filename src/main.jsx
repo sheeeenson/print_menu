d5c4f3fc09733f1dfaaa520b8669af17636c646b
Anton Sheenson <sheeeenson@gmail.com>
2026-06-01 14:09:23 +0400

    Load focused dish spacing stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx';
 import './styles.css';
+import './dishSpacing.css';
 
 createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
